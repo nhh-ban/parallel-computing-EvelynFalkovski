5b06b9ad31d1326828482b9ab54cbaed432b07bd
Evelyn Falkovski <evelyn.falkovski@student.nhh.no>
2023-10-28 13:47:04 +0200

    solved Problem 2

---
+++ timer.R
@@ -0,0 +1,16 @@
+library(tictoc)
+
+# Solution without changes
+tic()
+source("../scripts/solution_without_changes.R")
+toc()
+
+# Solution 2 - rewriting the loop
+tic()
+source("../scripts/solution_2.R")
+toc()
+
+# Solution 3 - rewriting MTweedieTests function
+tic()
+source("../scripts/solution_3.R")
+toc()
